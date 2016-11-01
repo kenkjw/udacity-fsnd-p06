3b555f6abd435f0ff02e7ec40c49d03384ee9d6d
Ken <kenkjw@gmail.com>
2016-11-01 12:14:24 -0700

    Add default places

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,20 @@ $(document).ready(function(){
     this.lng = ko.observable(data.lng);
   }
 
+  function getDefaultPlaces() {
+    return ko.observableArray([
+      new Place({name: "Phnom Penh Restaurant", lat: 49.278360, lng: -123.098231}),
+      new Place({name: "Guu Japanese Restaurant", lat: 49.284005, lng: -123.125435}),
+      new Place({name: "Rodney's Oyster House", lat: 49.274307, lng: -123.123136}),
+      new Place({name: "Landmark Hot Pot House", lat: 49.249836, lng: -123.115540}),
+      new Place({name: "Granville Island Brewing", lat: 49.270616, lng: -123.135774})
+    ]);
+  }
+
   function RecommendationViewModel() {
     var self = this;
+    var places = getDefaultPlaces()
 
-    
   }
 
   ko.applyBindings(new RecommendationViewModel())
