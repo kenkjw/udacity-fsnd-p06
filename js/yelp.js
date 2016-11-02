65d7838af8cac7ac94aa3c8de7e6260e85560f11
Ken <kenkjw@gmail.com>
2016-11-02 00:06:23 -0700

    Basic yelp api functionality

---
+++ js/yelp.js
@@ -0,0 +1,65 @@
+var _consumer_key = 'QXzwe4kuZzzFix0KCe7j-Q',
+_oauth_token = 'V2hGN6Wt5vDVHUJrhAbkseCqrWiFAkD1',
+_consumer_secret = 'NSq_TjXOYhcq4q5KW4p2igxN3SU',
+_token_secret = 'W-X_6POjlJaaoPM0HhdD8zFjV2s';
+
+function generateNonce() {
+    var length = 16;
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var result = '';
+    for(var i=0;i<length;i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+
+}
+function getSignature(id) {
+    var httpMethod = 'GET',
+    yelp_url = 'https://api.yelp.com/v2/business/' + id,
+    parameters = {
+        oauth_consumer_key : _consumer_key,
+        oauth_token : _oauth_token,
+        oauth_nonce : generateNonce(),
+        oauth_timestamp : '1191242096',
+        oauth_signature_method : 'HMAC-SHA1',
+        oauth_version : '1.0'
+    },
+    consumerSecret = _consumer_secret,
+    tokenSecret = _token_secret,
+    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+    encodedSignature = oauthSignature.generate(httpMethod, yelp_url, parameters, consumerSecret, tokenSecret);
+    return encodedSignature;
+
+}
+
+asdf = getSignature('asdf');
+
+function yelpGet(id) {
+    var url = 'https://api.yelp.com/v2/business/' + id;
+    var httpMethod = 'GET';
+    var parameters = {
+        oauth_consumer_key : _consumer_key,
+        oauth_token : _oauth_token,
+        oauth_nonce : generateNonce(),
+        oauth_timestamp : Math.floor(Date.now()/1000),
+        oauth_signature_method : 'HMAC-SHA1',
+        oauth_version : '1.0',
+        callback: 'cb'
+    }
+    encodedSignature = oauthSignature.generate(httpMethod, url, parameters, _consumer_secret, _token_secret);
+    parameters.oauth_signature = encodedSignature;
+    var settings = {
+        url: url,
+        data: parameters,
+        cache: true,
+        dataType: 'jsonp',
+        success: function(results) {
+            return results;
+        },
+        fail: function() {
+            alert('error');
+        }
+    }
+    $.ajax(settings);
+}
+yelpGet('phnom-penh-vancouver');
